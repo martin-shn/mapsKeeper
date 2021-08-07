fbc831451250922ca55e2809cc7612c6342b2812
Martin Shn <87691841+martin-shn@users.noreply.github.com>
2021-08-07 21:02:04 +0300

    final version

--- js/userPrefService.js
+++ js/userPrefService.js
@@ -1,4 +1,5 @@
 'use strict';
+var gUserPrefs;
 
 function onInit() {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
@@ -15,6 +16,37 @@ function onInit() {
         },
         false
     );
+
+    _loadUserPrefs();
+    document.querySelector('#name').value = gUserPrefs.userSet ? gUserPrefs.name : '';
+    document.querySelector('#theme').value = gUserPrefs.theme;
+    document.querySelector('#dob').value = gUserPrefs.dob;
+    document.querySelector('#email').value = gUserPrefs.email;
+    document.querySelector('#range').value = gUserPrefs.range;
+    onChangeRng(gUserPrefs.range);
+    changeUserPrefs();
+}
+
+function changeUserPrefs() {
+    let els = document.querySelectorAll('.userPrefChangable');
+    els.forEach(function (el) {
+        el.style.color = gUserPrefs.theme;
+    });
+}
+
+function _loadUserPrefs() {
+    gUserPrefs = loadFromStorage('userPrefs');
+    if (!gUserPrefs) {
+        gUserPrefs = {
+            userSet: false,
+            name: 'Stranger, <a href="user-prefs.html" style="color:#b7901b">let\'s setup your prefs</a>',
+            theme: '#ffc107',
+            dob: '1980-01-01',
+            email: '',
+            range: '25',
+        };
+    }
+    return gUserPrefs;
 }
 
 function onChangeRng(rng) {
@@ -22,14 +54,19 @@ function onChangeRng(rng) {
 }
 
 function saveForm() {
-    var userPrefs = {
+    gUserPrefs = {
+        userSet: true,
         name: document.querySelector('#name').value,
         theme: document.querySelector('#theme').value,
         dob: document.querySelector('#dob').value,
         email: document.querySelector('#email').value,
         range: document.querySelector('#range').value,
     };
-    saveToStorage('userPrefs', userPrefs);
-    document.querySelector('.hidden').classList.toggle('hidden');
-    document.querySelector('.hidden').classList.toggle('shown');
+    saveToStorage('userPrefs', gUserPrefs);
+    document.querySelector('.saved-msg').classList.remove('hidden');
+    setTimeout(() => {
+        document.querySelector('.saved-msg').classList.add('hidden');
+    }, 3000);
+
+    location.reload();
 }
